7b8b35790b86e7b0759a9780c06afac445500291
jhyokim03 <jhyokim03@naver.com>
2024-11-13 09:38:32 +0900

    Delete src directory

--- src/content.js
+++
@@ -1,9 +0,0 @@
-// 현재 페이지가 유튜브 페이지인지 확인
-if (window.location.host.includes("youtube.com")) {
-    const videoUrl = window.location.href;
-
-    // background.js에 메시지로 URL을 보냄
-    chrome.runtime.sendMessage({ type: "FETCH_NEWS", videoUrl: videoUrl }, (response) => {
-        console.log("관련 뉴스:", response.news);
-    });
-}
